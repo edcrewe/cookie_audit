454a2ffbc6f4e06f8f9ecdee47965058e8c433b6
Ed Crewe <edmundcrewe@gmail.com>
2011-07-22 14:10:51 +0100

    add in embedded

--- cookie_audit/spiders/domain_spider.py
+++ cookie_audit/spiders/domain_spider.py
@@ -1,9 +1,9 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
-from cookie_audit.settings import DOMAINS, URLS
-from cookie_audit.items import CrawlAuditItem
-from cookie_audit.pipelines import JsonWriterPipeline
+from settings import DOMAINS, URLS
+from items import CrawlAuditItem
+from pipelines import JsonWriterPipeline
 
 class DomainSpider(BaseSpider):
     """ Domain spider crawls any subdomain that ends with *.mydomain.com 
@@ -27,11 +27,35 @@ class DomainSpider(BaseSpider):
             if response.url in links:
                 links.remove(response.url)
             item['links'] = len(links)
-            self.pipe.process_item(item)
+            #flash, javascript and framesets can be external cookie sources
+            embed = hxs.select('//embed/@src').extract()
+            embed.extend(hxs.select('//object/@data').extract())
+            embed.extend(hxs.select('//script/@src').extract())
+            embed.extend(hxs.select('//frameset/@src').extract())
+            embed = set(embed)
+            for url in embed:
+                # Store embedded scripts / flash since also source of cookies
+                # can we save flash cookies? - maybe needs separate firefox grab of url
+                if url.startswith('/'):
+                    url = 'http://www.bris.ac.uk%s' % url
+                elif not url.startswith('http'):
+                    rurl = response.url
+                    if not rurl.endswith('/'):
+                        urlbits = response.url.split('/')
+                        rurl = '/'.join(urlbits[-1])
+                    url = '%s%s' % (rurl, url)
+                if not self.crawled(url):
+                    newresponse = Request(url)
+                    newitem = CrawlAuditItem()                
+                    newitem['url'] = response.url
+                    newitem['metatype'] = response.meta
+                    newitem['links'] = 0
+                    newitem.save()
             for url in links:
-                url = self.domain_check(url)
-                if url:
-                    yield Request(url, callback=self.parse) 
+                if not self.crawled(url):
+                    url = self.domain_check(url)
+                    if url:
+                        yield Request(url, callback=self.parse) 
             # Just save crawled pages not files/images
             try:
                 item.save()
@@ -49,10 +73,20 @@ class DomainSpider(BaseSpider):
         if url:
             if url.find('http')>-1:
                 url = url.split('/')[2]
-                for domain in self.yield_allowed_domains:        
-                    if url.endswith(domain):
-                        return original
                 for start in self.start_urls:
                     if url.startswith(start):
                         return original
+                for domain in self.yield_allowed_domains:        
+                    if url.endswith(domain):
+                        return original
         return ''
+
+    def crawled(self, url):
+        """ Dont recrawl same urls """
+        try:
+            item = CrawlAuditItem.get(url__exact=url)
+            if item:
+                return True
+        except:
+            pass
+        return False
